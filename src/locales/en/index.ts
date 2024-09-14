78dd8fe1aa2105c93c424ae94614890f80d86682
wladyslaw <vladislavvaliuchenkov@gmail.com>
2024-09-14 16:55:05 +0100

    added translations

--- src/locales/en/index.ts
+++ src/locales/en/index.ts
@@ -1,7 +1,9 @@
 import notFound from './not-found.json'
+import todos from './todos.json'
 import translation from './translation.json'
 
 export const en = {
 	translation: translation,
-	'not-found': notFound
+	'not-found': notFound,
+	todos: todos
 } as const
